46baa4c90f971770343cfa92b543ae4a504d37c5
황희수 <hhs@hwanghuisuui-Macmini.local>
2026-05-14 21:23:31 +0900

    feat(bulk-sms): 페이지 셸 + 탭 컨테이너 + 공용 배지

---
+++ dental-clinic-manager/src/app/dashboard/bulk-sms/page.tsx
@@ -0,0 +1,7 @@
+import BulkSmsManagement from '@/components/BulkSms/BulkSmsManagement'
+
+export const dynamic = 'force-dynamic'
+
+export default function BulkSmsPage() {
+  return <BulkSmsManagement />
+}
